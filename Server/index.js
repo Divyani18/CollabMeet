f9da3aa0bc57f533943d92c87320fe6dc761bdaa
Divyani <divyani018@gmail.com>
2021-02-01 16:55:37 -0500

    changed path to static files in server

--- Server/index.js
+++ Server/index.js
@@ -16,10 +16,10 @@ const wsServer = new webSocket({
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../Client/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../Client/build', '../Client/public/index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
