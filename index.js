238cc330ce7d4e1fdeae760be76ffd2ac6131379
Divyani <divyani018@gmail.com>
2021-02-02 02:58:30 -0500

    changes before deploying

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ const wsServer = new webSocket({
   httpServer : server
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const clients = {};
 
