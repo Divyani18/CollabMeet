4ebc3b544c2e9416b73b79af476f7167d71beb36
divyani <divyani018@gmail.com>
2024-10-03 22:41:45 +0530

    revert changes related to migration to vercel

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const wsServer = new webSocket({
   httpServer : server
 });
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'./client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const clients = {};
 
